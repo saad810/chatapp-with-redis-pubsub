930f3868483d2bf4a390c8a5f610676258e82380
saad810 <saadameen810@gmail.com>
2025-08-06 00:44:05 +0500

    socket init

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Server is running on port 3000');
+import http from 'http';
+import SocketService from './services/socket';
+
+async function init(){
+    const socketService = new SocketService();
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT || 3003;
+
+    socketService.io.attach(httpServer);
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+init();
