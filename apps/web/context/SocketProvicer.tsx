7fca49675380121baaf9bdef8c61292066246825
saad810 <saadameen810@gmail.com>
2025-08-06 01:56:52 +0500

    smited messages

--- apps/web/context/SocketProvicer.tsx
+++ apps/web/context/SocketProvicer.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useCallback, useEffect } from "react"
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 interface SocketProviderProps {
     children?: React.ReactNode;
@@ -22,13 +22,22 @@ export const useSocket = () => {
 };
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+    const [socket, setSocket] = React.useState<Socket | null>(null);
+
     const sendMessage: IsocketContext["sendMessage"] = useCallback((message) => {
         console.log("Sending message:", message);
-    }, []);
+        if (socket) {
+            socket.emit("event:message", { message });
+        } else {
+            console.error("Socket is not connected");
+        }
+    }, [socket]);
     useEffect(() => {
         const _socket = io("http://localhost:3003")
+        setSocket(_socket);
         return () => {
             _socket.disconnect();
+            setSocket(null);
         }
     }, []);
     return (
